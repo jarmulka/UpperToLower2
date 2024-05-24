9f84d4ab31fff29cf4f0752e4992ce5ed325d746
jeremi <jeremi.araszkiewicz@gmail.com>
2024-05-24 09:05:35 +0200

    added the menu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
-#include <iostream>
+#include <stdio.h>
+#include <ctype.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+    int choice;
+    char text[100] = "";
+
+    while (1) {
+        // Display the menu
+        printf("Menu:\n");
+        printf("1. Enter text\n");
+        printf("2. Display the text\n");
+        printf("3. Change every single letter in the text to lower case\n");
+        printf("4. Change every single letter to upper case\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
